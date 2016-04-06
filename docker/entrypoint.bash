93d8f39c68e62b12bb4fe5aa2d172d6f50d5222a
daniel <daniel_van_meyer@seotecs.de>
2016-04-06 17:40:29 +0200

    updated plugins; added creation of packages_own.txt

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -2,12 +2,16 @@
 
 #echo "### starting entrypoint.bash ###"
 
-#atom.io
+#atom.io innstall plugins
 if [ ! -d /root/.atom/packages/file-icons ]
 then
     apm install --packages-file /mapped/packages.txt
 fi
 
+#atom.io generate packages_own.txt
+chmod -R 777 /mapped
+rm /mapped/packages_own.txt
+apm list --installed --bare > /mapped/packages_own.txt
 
 chmod -R 777 /root/.atom
 
